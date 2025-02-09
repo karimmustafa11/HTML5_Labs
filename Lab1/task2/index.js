cfc80256b0cab297dbaa760f19896ca074dc0c2b
karimmustafa <karimmustafa495@gmail.com>
2025-02-09 17:48:07 +0200

    Choose your color

---
+++ Lab1/task2/index.js
@@ -0,0 +1,13 @@
+//^ get elements
+let red = document.getElementById('red')
+let green = document.getElementById('green')
+let blue = document.getElementById('blue')
+let h3 = document.getElementsByTagName('h3')[0]
+let colors = [red, green, blue]
+
+//^ add input event to colors
+colors.forEach(color => {
+    color.addEventListener('input', function () {
+        h3.style.color = `rgb(${red.value},${green.value},${blue.value})`
+    })
+})
